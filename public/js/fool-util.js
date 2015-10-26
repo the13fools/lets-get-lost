df01f42bf1e61ea21a5f53726945dff6ed0be235
Josh Vekhter <josh@formlabs.com>
2015-10-25 23:44:50 -0400

    abstract loading

---
+++ public/js/fool-util.js
@@ -0,0 +1,37 @@
+initEditor = function(id) {
+	// Init ace editor
+	var editor = ace.edit(id);
+	editor.setTheme("ace/theme/tomorrow");
+	editor.session.setMode("ace/mode/javascript");
+	editor.renderer.setScrollMargin(10, 10);
+	editor.setOptions({
+	    // "scrollPastEnd": 0.8,
+	    autoScrollEditorIntoView: true,
+	    vScrollBarAlwaysVisible: false
+
+	});
+
+	// Control buttons
+	$('#' + id).parent().append(
+	'<div class="editor-control">' +
+		'<div class="editor-run ' + id + '">' +
+			'<span class="el el-caret-right"></span> Run' +
+		'</div>' +
+		'<div class="editor-expand ' + id + '">' +
+			'Resize <span class="el el-resize-full"></span>' +
+		'</div>' +
+	'</div>');
+
+	// Connect buttons
+	$( "." + id + ".editor-expand" ).click(function() {
+	  	$( "#" + id ).toggleClass( "editor-big", 500, "easeOutSine" )
+	  	.promise().done(function(){
+	  		var dom = ace.require("ace/lib/dom");
+	  		ace.edit(id).resize();
+	  	});
+	});
+
+	$( "." + id + ".editor-run" ).click(function() {
+		eval(editor.getValue());
+	});
+}
